4daeaa19cc56db861d9f9a70cbcb7e32011a90d9
Polapob <71330294+Polapob@users.noreply.github.com>
2023-10-02 10:57:47 +0700

    refactor: remove unused import

--- src/main/java/SWA/microservice/first/controller/TopicController.java
+++ src/main/java/SWA/microservice/first/controller/TopicController.java
@@ -1,26 +1,12 @@
 package SWA.microservice.first.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Aggregates;
-import SWA.microservice.first.Driven.MongoDB.MongoClientConnection;
-import SWA.microservice.first.domain.Comment;
 import SWA.microservice.first.domain.Forum;
 import SWA.microservice.first.domain.Topic;
 import SWA.microservice.first.dto.topic.CreateTopicRequestDTO;
@@ -32,23 +18,30 @@ import SWA.microservice.first.service.ITopicService;
 @RestController
 public class TopicController {
 	
+	@Autowired
 	private ITopicService topicService;
 	
 	@GetMapping("/topics")
 	public GetTopicsResponseDTO getTopics() throws Exception {
-		return topicService.getTopics();
+		var topics = topicService.getTopics();
+		GetTopicsResponseDTO response = new GetTopicsResponseDTO(topics, true);
+		return response;
 	}
 
 	@PostMapping("/topics")
 	public CreateTopicResponseDTO createTopic(@RequestBody CreateTopicRequestDTO request) throws Exception {
 		var forum = new Forum(request.getSubjectId(), request.getYear(), request.getSemester(), request.getSection());
-		var topic = new Topic(request.getDescription(), forum, request.getCreatorId());
-		return topicService.createTopic(topic);
+		var _topic = new Topic(request.getDescription(), forum, request.getCreatorId());
+		
+		var topic = topicService.createTopic(_topic);
+		var response = new CreateTopicResponseDTO(topic,true);
+		return response;
 	}
 
 	@GetMapping("/topics/{id}")
 	public GetTopicResponseDTO getTopicById(@PathVariable("id") String id) throws Exception {
-		return topicService.getTopicById(id);
+		var topic = topicService.getTopicById(id);
+		return topic;
 	}
 
 	@GetMapping("/health-check")

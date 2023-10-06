93cd1f8d034856fee59ce8514a2d915f53918584
Polapob <71330294+Polapob@users.noreply.github.com>
2023-10-06 08:05:17 +0700

    fix: fix runtime bugs

--- src/main/java/SWA/microservice/first/controller/CommentController.java
+++ src/main/java/SWA/microservice/first/controller/CommentController.java
@@ -4,12 +4,13 @@ package SWA.microservice.first.controller;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import SWA.microservice.first.domain.Comment;
 import SWA.microservice.first.dto.comment.CreateCommentRequestDTO;
@@ -18,14 +19,14 @@ import SWA.microservice.first.dto.comment.GetCommentByIdResponseDTO;
 import SWA.microservice.first.dto.comment.GetCommentsByTopicIdResponseDTO;
 import SWA.microservice.first.service.ICommentService;
 
-@Controller
+@RestController
 @RequestMapping("/comments")
 public class CommentController {
 	@Autowired
 	private ICommentService commentService;
 	
-	@PostMapping 
-	public CreateCommentResponseDTO createComment(CreateCommentRequestDTO request) throws Exception {
+	@PostMapping("")
+	public CreateCommentResponseDTO createComment(@RequestBody CreateCommentRequestDTO request) throws Exception {
 		var comment = new Comment(request.topicId,request.description,request.authorId,new Date());
 		var commentDTO = commentService.createComment(comment);
 		var response = new CreateCommentResponseDTO(commentDTO,true);

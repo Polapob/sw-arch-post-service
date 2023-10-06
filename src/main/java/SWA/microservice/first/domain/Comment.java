9e2e60c5da0c2e4af8a87f7e638cd401b400023e
Polapob <71330294+Polapob@users.noreply.github.com>
2023-10-06 07:44:46 +0700

    feat: implement comment repository

--- src/main/java/SWA/microservice/first/domain/Comment.java
+++ src/main/java/SWA/microservice/first/domain/Comment.java
@@ -1,51 +1,40 @@
 package SWA.microservice.first.domain;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Comment {
 	private String id;
 	private String topicId;
-	private Forum forum;
 	private String description;
-	private String commenterId;
+	private String authorId;
 	private Date createAt;
-
-	public Comment(String id, String topicId, Forum forum, String description, String commenterId, Date createAt) {
+	
+	public Comment(String topicId, String description, String authorId, Date createAt) {
+		this.id = UUID.randomUUID().toString();
 		this.topicId = topicId;
-		this.forum = forum;
 		this.description = description;
-		this.commenterId = commenterId;
+		this.authorId = authorId;
 		this.createAt = createAt;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public String getTopicId() {
 		return topicId;
 	}
-
-	public Forum getForum() {
-		return forum;
-	}
-
 	public String getDescription() {
 		return description;
 	}
-
-	public String getCommenterId() {
-		return commenterId;
+	public String getAuthorId() {
+		return authorId;
 	}
-
 	public Date getCreateAt() {
 		return createAt;
 	}
-
 	@Override
 	public String toString() {
-		return "Comment [id=" + id + ", topicId=" + topicId + ", forum=" + forum + ", description=" + description
-				+ ", commenterId=" + commenterId + ", createAt=" + createAt + "]";
-	}
-
+		return "Comment [id=" + id + ", topic=" + topicId + ", description=" + description + ", commenterId="
+				+ authorId + ", createAt=" + createAt + "]";
+	}	
 }
--- src/main/java/SWA/microservice/first/repository/CommentRepository.java
+++ src/main/java/SWA/microservice/first/repository/CommentRepository.java
@@ -1,5 +1,61 @@
 package SWA.microservice.first.repository;
 
-public class CommentRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+
+import SWA.microservice.first.Driven.MongoDB.IMongoClientConnection;
+import SWA.microservice.first.domain.Comment;
+
+@Repository
+public class CommentRepository implements ICommentRepository {
+	@Autowired
+	private IMongoClientConnection mongoClient;
+
+	public CommentRepository(IMongoClientConnection mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+
+	public Document createComment(Comment comment) throws Exception {
+		var collection = createCollection();
+		try {
+
+			var document = new Document("id", comment.getId()).append("topicId", comment.getTopicId())
+					.append("description", comment.getDescription()).append("authorId", comment.getAuthorId())
+					.append("createAt", comment.getCreateAt());
+
+			collection.insertOne(document);
+			return document;
+		} catch (MongoException ex) {
+			throw ex;
+		}
+
+	}
+
+	public Document getCommentById(String id) throws Exception {
+		var collection = createCollection();
+		var document = new Document("id", id);
+		var comment = collection.find(document).first();
+		return comment;
+	}
+
+	public List<Document> getCommentListByTopicId(String id) throws Exception {
+		var collection = createCollection();
+		var document = new Document("topicId", id);
+		var comments = collection.find(document).into(new ArrayList<>());
+		return comments;
+	}
+
+	private MongoCollection<Document> createCollection() throws Exception {
+		var client = mongoClient.createMongoClient();
+		var db = client.getDatabase("local");
+		var collection = db.getCollection("comments");
+		return collection;
+	}
 }

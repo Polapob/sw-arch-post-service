b27c5fc331b99a7ed3baba55fd233a9b48990e45
Polapob <71330294+Polapob@users.noreply.github.com>
2023-10-15 11:38:38 +0700

    feat: update dto in publish comment endpoint

---
+++ src/main/java/SWA/microservice/first/dto/comment/PublishCommentEventRequestDTO.java
@@ -0,0 +1,8 @@
+package SWA.microservice.first.dto.comment;
+
+public class PublishCommentEventRequestDTO {
+	public String correlationId;
+	public String authorId;
+	public String topicId;
+	public String description;
+}
--- src/main/java/SWA/microservice/first/service/ICommentService.java
+++ src/main/java/SWA/microservice/first/service/ICommentService.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import SWA.microservice.first.domain.Comment;
 import SWA.microservice.first.dto.comment.CommentDTO;
+import SWA.microservice.first.event.CreateCommentEvent;
 
 public interface ICommentService {
 	CommentDTO createComment(Comment comment) throws Exception;
 	List<CommentDTO> getCommentsByTopicId(String id) throws Exception;
 	CommentDTO getCommentById(String id) throws Exception;
-	boolean publishCreateCommentMessage() throws Exception;
+	boolean publishCreateCommentMessage(CreateCommentEvent event) throws Exception;
 }
